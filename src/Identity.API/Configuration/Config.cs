a02edc209cc30f9e1d27056b01dc0b517212b9ce
PvlD <paveldoul@yahoo.com>
2026-05-19 15:51:48 -0400

    update

--- src/Identity.API/Configuration/Config.cs
+++ src/Identity.API/Configuration/Config.cs
@@ -77,6 +77,41 @@ namespace eShop.Identity.API.Configuration
                 },
                 new Client
                 {
+                    ClientId = "webapprs31",
+                    ClientName = "WebApprs31 Client",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    ClientUri = "http://localhost:3010",                             // public uri of the client
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowAccessTokensViaBrowser = false,
+                    RequireConsent = false,
+                    AllowOfflineAccess = true,
+                    AlwaysIncludeUserClaimsInIdToken = true,
+                    RequirePkce = false,
+                    RedirectUris = new List<string>
+                    {
+                        "http://127.0.0.1:3010/user/signin-oidc",
+                        "http://localhost:3010/user/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "http://localhost:3010/signout-callback-oidc"
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "orders",
+                        "basket"
+                    },
+                    AccessTokenLifetime = 60*60*2, // 2 hours
+                    IdentityTokenLifetime= 60*60*2 // 2 hours
+                },
+                new Client
+                {
                     ClientId = "webapp",
                     ClientName = "WebApp Client",
                     ClientSecrets = new List<Secret>
--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -131,6 +131,36 @@ builder.AddExecutable("WebAppRs", "cargo", "..\\web-app-rs\\web-app-ws", "leptos
     .WithReference(rabbitMq)
     .WithEnvironment("IdentityUrl", identityEndpoint);
 
+/*
+builder.AddExecutable("WebAppRsA", "cargo", "..\\web-app-rs\\web-app-ws", "leptos", "watch")
+      .WithEnvironment(context =>
+      {
+          context.EnvironmentVariables["LEPTOS_SITE_ADDR"] = "127.0.0.1:3010";
+
+          context.EnvironmentVariables["AMQP_EXCHANGE_NAME"] = "eshop_event_bus";
+          context.EnvironmentVariables["AMQP_QUEUE_NAME"] = "Ordering.webapprs31";
+
+          context.EnvironmentVariables["OPENID_CLIENT_ID"] = "webapprs31";
+          context.EnvironmentVariables["OPENID_CLIENT_SECRET"] = "secret";
+          context.EnvironmentVariables["OPENID_REDIRECT_URL"] = "/user/signin-oidc";
+
+      })
+      .WithEndpoint("http", e =>
+      {
+          e.IsExternal = false;
+          e.IsProxied = false;
+          e.UriScheme = "http";
+          e.TargetHost = "127.0.0.1";
+          e.TargetPort = 3010;
+
+      }
+      )
+     .WithReference(basketApi)
+    .WithReference(catalogApi)
+    .WithReference(orderingApi)
+    .WithReference(rabbitMq)
+    .WithEnvironment("IdentityUrl", identityEndpoint);
+*/
 
 
 
